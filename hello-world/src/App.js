3d8445485b0d445f295b9cbdbe3e540fada946cc
Namratha Rao <nam@Namrathas-MacBook-Air.local>
2024-05-16 00:29:55 -0400

    Understanding props and reusability

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Hello from './components/Hello';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <Greet name="Khaleesi" actorName="Emilia Clarke">
+        <p>
+          This is the queen
+        </p>
+      </Greet>
+      <Greet name="Jon Snow" actorName="Kit Harrington">
+        <button>The King in the North </button>
+      </Greet>
+      <Greet name="Arya Stark" actorName="Maise Williams"/>
+
+      <Welcome name="Khaleesi" actorName="Emilia Clarke"/>
+      <Welcome name="Jon Snow" actorName="Kit Harrington"></Welcome>
+      <Welcome name="Arya Stark" actorName="Maise Williams"/>
+
+      {/* <Hello/> */}
     </div>
   );
 }
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -4,6 +4,17 @@ import React from 'react'
 //     return <h1>Hello World!</h1>
 // }
 
-const Greet = () => <h1>Hello World !!</h1>
+const Greet = (props) => {
+    console.log("props are", props);
+   return (
+    <div>
+        <h1>Hello {props.name} a.k.a {props.actorName} !!</h1>
+        {props.children}
+    </div>
+   )
+
+   // {} is a feature of JSX that transforms JSX.
+    
+}
 
 export default Greet;
