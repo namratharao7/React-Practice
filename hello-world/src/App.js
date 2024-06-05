583a3c85ad8d2bc9561b7d869ca4086f3573905e
Namratha Rao <nam@Namrathas-MacBook-Air.local>
2024-06-05 00:17:29 -0400

    laerning about event binding + passing message as props + conditional rendering

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,11 +4,16 @@ import Greet from './components/Greet';
 import Hello from './components/Hello';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Parent from './components/Parent';
+import UserGreeting from './components/UserGreeting';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      {/* <Message /> */}
+      {/* <Parent /> */}
+
+      <UserGreeting />
 
       {/* <Greet name="Khaleesi" actorName="Emilia Clarke">
         <p>
